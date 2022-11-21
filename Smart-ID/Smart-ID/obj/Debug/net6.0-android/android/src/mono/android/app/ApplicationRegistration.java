3c7a403fc4c79ac154e764d79e7606b799d42583
Umair Ahmed <86409567+ahmedumair1122@users.noreply.github.com>
2022-11-21 23:54:42 +0500

    features1

---
+++ Smart-ID/Smart-ID/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Smart_ID.MainApplication, Smart-ID, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6414910577c5cd1d33.MainApplication.class, crc6414910577c5cd1d33.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
