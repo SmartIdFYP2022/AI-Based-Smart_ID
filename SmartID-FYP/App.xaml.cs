9dccdf8af5120f2ccc78fa3826cd38c2e6ffe224
Naveed Qamar <naveedqamar6500@gmail.com>
2022-11-21 20:53:35 +0500

    Project Added

---
+++ SmartID-FYP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartID_FYP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
