474e84ced96ed84aea33221d7b4eee06ebd21909
Umair Ahmed <86409567+ahmedumair1122@users.noreply.github.com>
2022-11-23 01:08:13 +0500

    image

--- Smart-ID/Smart-ID/obj/Debug/net6.0-android/designtime/Resource.designer.cs
+++ Smart-ID/Smart-ID/obj/Debug/net6.0-android/designtime/Resource.designer.cs
@@ -7890,16 +7890,19 @@ namespace Smart_ID
 			public const int splash = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int test_custom_background = 2131165340;
+			public const int sprofileimage = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int test_level_drawable = 2131165341;
+			public const int test_custom_background = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int tooltip_frame_dark = 2131165342;
+			public const int test_level_drawable = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_light = 2131165343;
+			public const int tooltip_frame_dark = 2131165343;
+			
+			// aapt resource value: 0x7F0700A0
+			public const int tooltip_frame_light = 2131165344;
 			
 			static Drawable()
 			{
